b0da01409befa604682acf8c4291fc4f2c575b30
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2017-11-08 14:37:14 -0800

    added get and post routes to server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -28,9 +29,18 @@ app.get('/api/v1/books', (req, res) => {
 
 
 app.get('/api/v1/books/:id', (req, res) => {
-  client.query(`SELECT book_id, title, author, image_url, isbn FROM books WHERE book_id=1;`),
-    [req.params.book_id]
+  client.query(`SELECT * FROM books WHERE book_id=${req.params.id}`)
     .then (results => res.send(results.rows))
+    .catch(console.error);
+});
+
+app.post('/api/v1/books', bodyParser, (req, res) => {
+  let {title, author, isbn, image_url, description} = req.body;
+  client.query(
+    `INSERT INTO books title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5)`,
+    [title, author, isbn, image_url, description]
+  )
+    .then(results => res.sendStatus(201))
     .catch(console.error);
 });
 
