aedf88db46ed76c126a9c6343c77c6e57310ddc1
Joel Clair <clair.joel@yahoo.com>
2017-11-09 13:22:15 -0800

    heroku predeploy

--- server.js
+++ server.js
@@ -33,15 +33,16 @@ app.get('/api/v1/books/:id', (req, res) => {
     .catch(console.error)
 })
 
-// app.post('/api/v1/books', bodyParser, (req, res) => {
-//   let {title, author, isbn, image_url, description} = req.body;
-//   client.query(`
-//     INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5)`
-//     [title, author, isbn, image_url, description]
-//   )
-//     .then( results => res.sendStatus(201))
-//     .catch(console.error)
-// })
+app.post('/api/v1/books', bodyParser, (req, res) => {
+  let {title, author, isbn, image_url, description} = req.body;
+  client.query(`
+    INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5)`,
+    [title, author, isbn, image_url, description]
+  )
+    .then( results => res.sendStatus(201))
+    .catch(console.error)
+})
+
 app.get('*', (req, res) =>res.redirect(CLIENT_URL));
 
 //loadDB();
